2db552871325c2d5b6de114cf224c51d8acc83d7
Veeti Rajaniemi <101036934+veetirajaniemi@users.noreply.github.com>
2026-05-27 20:37:16 +0300

    Add files via upload

---
+++ scripts/gold/ddl_gold.sql
@@ -0,0 +1,85 @@
+/*
+===============================================
+DDL: Creating gold views
+===============================================
+
+Script purpose:
+	This script creates views for the gold layer in the data warehouse.
+	The gold layer includes the final dimensions and fact tables (Star Schema).
+
+	Each view performs transformations combining data from the silver layer
+	to produce a clean, business-ready dataset. 
+*/
+
+
+IF OBJECT_ID('gold.dim_customers', 'V') IS NOT NULL
+	DROP VIEW gold.dim_customers;
+GO
+
+CREATE VIEW gold.dim_customers AS
+SELECT
+	ROW_NUMBER() OVER (ORDER BY ci.cst_id) AS customer_key,
+	ci.cst_id AS customer_id,
+	ci.cst_key AS customer_number,
+	ci.cst_firstname AS first_name,
+	ci.cst_lastname AS last_name,
+	li.cst_country AS country,
+	ci.cst_marital_status AS marital_status,
+	CASE WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr -- CRM is the master source for gender :)
+		ELSE COALESCE(ci2.cst_gender, 'n/a')
+	END AS gender,
+	ci2.cst_bd_dt AS birthdate,
+	ci.cst_create_date AS create_date
+FROM silver.crm_cst_info ci
+LEFT JOIN silver.erp_cst_info ci2
+ON	      ci.cst_key = ci2.cst_id
+LEFT JOIN silver.erp_loc_info li
+ON        ci.cst_key = li.cst_id
+
+
+IF OBJECT_ID('gold.dim_products', 'V') IS NOT NULL
+	DROP VIEW gold.dim_products;
+GO
+
+CREATE VIEW gold.dim_products AS 
+SELECT
+	ROW_NUMBER() OVER (ORDER BY pin.prd_start_dt, pin.prd_key) AS product_key,
+	pin.prd_id AS product_id,
+	pin.prd_key AS product_number,
+	pin.prd_nm AS product_name,
+	pin.cat_id AS category_id,
+	pin2.prd_cat AS category,
+	pin2.prd_subcat AS subcategory,
+	pin2.prd_maintenance AS maintenance,
+	pin.prd_cost AS cost,
+	pin.prd_line AS line,
+	pin.prd_start_dt AS start_date
+FROM silver.crm_prd_info pin
+LEFT JOIN silver.erp_prd_info AS pin2
+ON pin.cat_id = pin2.prd_id
+WHERE pin.prd_end_dt IS NULL -- filtering out historical data!
+
+
+IF OBJECT_ID('gold.fact_sales', 'V') IS NOT NULL
+	DROP VIEW gold.fact_sales;
+GO
+
+CREATE VIEW gold.fact_sales AS
+SELECT
+	sd.sls_ord_num AS order_number,
+	pr.product_number AS product_key,
+	cu.customer_key,
+	sd.sls_order_dt AS order_date,
+	sd.sls_ship_dt AS shipping_date,
+	sd.sls_due_dt AS due_date,
+	sd.sls_sales AS sales_amount,
+	sd.sls_quantity AS quantity,
+	sd.sls_price AS price
+FROM silver.crm_sales_details sd
+LEFT JOIN gold.dim_products AS pr
+ON sd.sls_prd_key = pr.product_number
+LEFT JOIN gold.dim_customers AS cu
+ON sd.sls_cst_id = cu.customer_id
+
+
+
